09940c42a11b3fb8fd12a1f189913d5a8d1c3984
nanasemai <42890263+nanasemai@users.noreply.github.com>
2025-11-16 23:30:46 +0800

    UBUNTU:oneplus6 init

--- debian.qcom/rules.d/arm64.mk
+++ debian.qcom/rules.d/arm64.mk
@@ -1,7 +1,7 @@
 human_arch	= ARMv8
 build_arch	= arm64
 defconfig	= defconfig
-flavours	= qcom
+flavours	= qcom oneplus6
 build_image	= Image.gz
 kernel_file	= arch/$(build_arch)/boot/Image.gz
 install_file	= vmlinuz
@@ -16,3 +16,10 @@ do_tools_perf_jvmti = true
 do_tools_bpftool = true
 
 do_dtbs		= true
+
+# 一加6特定配置
+ifeq ($(flavour),oneplus6)
+    configs += oneplus6_defconfig
+    kernel_image = Image.gz-dtb
+    dtbs = sdm845-oneplus-enchilada.dtb sdm845-oneplus-fajita.dtb
+endif
